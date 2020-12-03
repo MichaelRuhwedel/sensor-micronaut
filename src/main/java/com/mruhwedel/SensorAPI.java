a6e847bc552c3ef66c8508a8c4fe706db2f0d306
MichaelRuhwedel <michael.ruhwedel@gmail.com>
2020-12-03 14:02:09 +0100

    feat(sensor-api): cleaned up log messages

--- src/main/java/com/mruhwedel/SensorAPI.java
+++ src/main/java/com/mruhwedel/SensorAPI.java
@@ -3,24 +3,33 @@ package com.mruhwedel;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
 import lombok.Data;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.ZonedDateTime;
 
+import static io.micronaut.http.HttpStatus.CREATED;
+
 @Controller("/api/v1/sensors/{uuid}")
+@Slf4j
 public class SensorAPI {
 
     @Get // media-type defaults to application/json
-    public String status(@QueryValue("uuid") String uuid) {
-        return "{\"status\" : \"OK\"}";
+    public StatusDto status(@QueryValue("uuid") String uuid) {
+
+        StatusDto statusDto = new StatusDto(StatusDto.Status.OK);
+        log.info("{}: {}", uuid, statusDto.getStatus());
+        return statusDto;
     }
 
     @Post("/measurements")
-    @Status(HttpStatus.CREATED)
+    @Status(CREATED)
     public void measurements(
             @QueryValue("uuid") String uuid,
             @Body Measurements measurements
     ) {
-
+        log.info("{}: {}@{}",
+                uuid, measurements.getTime(), measurements.getCo2());
     }
 
     @Data
@@ -29,4 +38,14 @@ public class SensorAPI {
         private ZonedDateTime time;
     }
 
+    @Value
+    private static class StatusDto {
+        private enum Status {
+            OK //,
+            // ALERT,
+            /// WARNING
+        }
+
+        Status status;
+    }
 }

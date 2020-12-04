8a6cc0fa1a593fc8cadfb4223802988aba763366
MichaelRuhwedel <michael.ruhwedel@gmail.com>
2020-12-04 14:59:33 +0100

    feat(db) query and write (might work)

--- src/main/java/com/mruhwedel/InfluxDbSensorRepository.java
+++ src/main/java/com/mruhwedel/InfluxDbSensorRepository.java
@@ -1,16 +1,30 @@
 package com.mruhwedel;
 
+import com.google.common.base.Strings;
+import com.google.common.escape.Escaper;
+import com.google.common.escape.Escapers;
+import com.influxdb.annotations.Column;
 import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.domain.Query;
+import com.influxdb.client.domain.WritePrecision;
+import com.influxdb.client.write.Point;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static com.influxdb.client.domain.WritePrecision.S;
 import static com.mruhwedel.SensorStatus.OK;
 
 @Slf4j
@@ -18,23 +32,78 @@ import static com.mruhwedel.SensorStatus.OK;
 @RequiredArgsConstructor // if there's only one, it'll be used for injection
 public class InfluxDbSensorRepository implements SensorRepository {
 
+    private static final WritePrecision WRITE_PRECISION = S; // the specs say that's all we need
+    public static final String MEASUREMENT_NAME = "co2";
+    public static final int LIMIT_PREVIOUS = 2;
+
     @SuppressWarnings("unused") // @injected
     private final InfluxDBClient influxDBClient;
 
     @Override
     public Optional<SensorStatus> readStatus(@NonNull String uuid) {
-        log.info("reading");
-        return Optional.of(OK);
+        String query = createQuery(uuid, 1);
+
+        Optional<SensorStatus> status = influxDBClient.getQueryApi()
+                .query(query).stream()
+                .flatMap(t -> t.getRecords().stream())
+                .findFirst()
+                .map(fr -> (String) fr.getValueByKey("status"))
+                .map(SensorStatus::valueOf);
+
+        log.info("Read {}", status);
+        return status;
+    }
+
+    @NotNull
+    private String createQuery(String uuid, int limit) {
+        return String.format(
+                "data \n" +
+                        "  |> sort(columns: [\"_time\"]) \n" +
+                        "  |> filter(fn: (r) => r.uuid == %s) \n" +
+                        "  |> limit(n: %d)",
+                uuid, limit
+        );
     }
 
     @Override
     public @NonNull List<QualifiedMeasurement> fetchTwoPreviousMeasurements(@NonNull String uuid) {
-        log.info("fetching");
-        return Collections.emptyList();
+        String query = createQuery(uuid, LIMIT_PREVIOUS);
+
+        List<QualifiedMeasurement> status = influxDBClient.getQueryApi()
+                .query(query).stream()
+                .flatMap(t -> t.getRecords().stream())
+                .filter(Objects::nonNull)
+                .limit(LIMIT_PREVIOUS) // redundant, just in case the query would return more
+                .map(fr -> new QualifiedMeasurement(
+                        new Measurement(
+                                (int) fr.getValue(),
+                                ZonedDateTime.ofInstant(fr.getTime(), ZoneId.systemDefault())
+                        ),
+                        SensorStatus.valueOf((String) fr.getValueByKey("status"))
+                ))
+                .collect(Collectors.toList());
+        log.info("{}: {} of up to {} measurements in repository",
+                uuid, status.size(), LIMIT_PREVIOUS);
+
+        return status;
+
+
     }
 
     @Override
     public void record(@NonNull String uuid, QualifiedMeasurement qualifiedMeasurement) {
         log.info("recording");
+        Instant time = qualifiedMeasurement.getMeasurement().getTime().toInstant();
+
+        Point co2Point = Point
+                .measurement(MEASUREMENT_NAME)
+                .addField("uuid", uuid)
+                .addField("status", qualifiedMeasurement.getSensorStatus().name())
+                .addField("value", qualifiedMeasurement.getMeasurement().getCo2())
+                .time(time, WRITE_PRECISION);
+
+        influxDBClient.getWriteApi().writePoint(co2Point);
+
     }
+
 }

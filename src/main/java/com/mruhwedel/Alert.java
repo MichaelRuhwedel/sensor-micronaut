5f574922dfabcb010d747196cbb67abbdfa0f231
MichaelRuhwedel <michael.ruhwedel@gmail.com>
2020-12-07 09:46:33 +0100

    doc(alert) why and when

--- src/main/java/com/mruhwedel/Alert.java
+++ src/main/java/com/mruhwedel/Alert.java
@@ -6,14 +6,42 @@ import lombok.NoArgsConstructor;
 
 import java.time.ZonedDateTime;
 
+/**
+ * When enough consecutive measurements are above the warn threshold an
+ * {@code Alarm} will be recorded.
+ *
+ * @see Alert#LIMIT_FOR_ALARM
+ * @see Measurement#CO_PPM_WARN_THRESHOLD
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Alert {
+    public static final int LIMIT_FOR_ALARM = 3;
+
+    /**
+     * When the alarm started, the timestamp of measurement3
+     */
     private ZonedDateTime startTime;
+
+    /**
+     * When the alarm started ended, the timestamp of the 3rd measurement below threshold
+     */
     private ZonedDateTime endTime;
+
+    /**
+     * the earliest measurement that lead on to the alarm
+     */
     private int measurement1;
+
+    /**
+     * the second to last measurement that lead on to the alarm
+     */
     private int measurement2;
+
+    /**
+     * the ultimate measurement that triggered the alarm
+     */
     private int measurement3;
 
     boolean isOngoing() {

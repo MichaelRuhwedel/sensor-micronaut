e0d660db601dd5ebf14c277e089af172796082fa
MichaelRuhwedel <michael.ruhwedel@gmail.com>
2020-12-07 10:22:59 +0100

    refactor(application) remove dead arguments

--- src/main/java/com/mruhwedel/application/Application.java
+++ src/main/java/com/mruhwedel/application/Application.java
@@ -1,17 +1,9 @@
 package com.mruhwedel.application;
 
-import io.micronaut.context.annotation.Factory;
 import io.micronaut.runtime.Micronaut;
 
-@Factory
 public class Application {
-
     public static void main(String[] args) {
-        Micronaut
-                .build(args)
-                .mainClass(Application.class)
-                .defaultEnvironments("dev")
-                .start();
+        Micronaut.run(Application.class, args);
     }
-
 }

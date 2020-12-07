74c6427e9d4ed77e559055a15e40be05377a6635
MichaelRuhwedel <michael.ruhwedel@gmail.com>
2020-12-07 11:12:54 +0100

    test(sensor-service) when NOT to alert

--- src/test/groovy/com/mruhwedel/domain/SensorServiceSpec.groovy
+++ src/test/groovy/com/mruhwedel/domain/SensorServiceSpec.groovy
@@ -3,10 +3,23 @@ package com.mruhwedel.domain
 import com.mruhwedel.StatusCalculator
 import com.mruhwedel.repository.AlertRepository
 import com.mruhwedel.repository.SensorMeasurementRepository
+import org.influxdb.annotation.Measurement
 import spock.lang.Specification
+import spock.lang.Unroll
 
 import static com.mruhwedel.domain.SensorStatus.*
 import static com.mruhwedel.domain.SensorTestData.*
+import static com.mruhwedel.domain.SensorTestData.ANY_UUID
+import static com.mruhwedel.domain.SensorTestData.ANY_UUID
+import static com.mruhwedel.domain.SensorTestData.ANY_UUID
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_ABOVE_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_ABOVE_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_ABOVE_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_BELOW_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_BELOW_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_BELOW_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_BELOW_THRESHOLD
+import static com.mruhwedel.domain.SensorTestData.MEASUREMENT_BELOW_THRESHOLD
 
 class SensorServiceSpec extends Specification {
 
@@ -22,7 +35,7 @@ class SensorServiceSpec extends Specification {
 
         then:
         1 * service.alertRepository.getLatestOngoing(ANY_UUID) >> alert
-        1 * service.sensorMeasurementRepository.fetchCurrent(ANY_UUID) >> MEASUREMENT_BELOW_THRESHOLD
+        1 * service.sensorMeasurementRepository.fetchCurrent(ANY_UUID) >> Optional.of(MEASUREMENT_BELOW_THRESHOLD)
 
         status
                 .map(OK::equals)
@@ -41,7 +54,7 @@ class SensorServiceSpec extends Specification {
 
         then:
         1 * service.alertRepository.getLatestOngoing(ANY_UUID) >> Optional.empty()
-        1 * service.sensorMeasurementRepository.fetchCurrent(ANY_UUID) >> MEASUREMENT_ABOVE_THRESHOLD
+        1 * service.sensorMeasurementRepository.fetchCurrent(ANY_UUID) >> Optional.of(MEASUREMENT_ABOVE_THRESHOLD)
 
         status
                 .map(WARN::equals)
@@ -67,19 +80,39 @@ class SensorServiceSpec extends Specification {
                 .orElseThrow()
     }
 
-    def "recordAndUpdateStatus() will record the measurement and the correct status"() {
+    def "recordAndUpdateAlert() will collect the measurement"() {
         given:
         def currentMeasurement = MEASUREMENT_BELOW_THRESHOLD
-        def currentStatus = new QualifiedMeasurement(currentMeasurement, OK)
-        def previousMeasurements = []
 
         when:
         service.recordAndUpdateAlert(ANY_UUID, currentMeasurement)
 
         then:
-        1 * service.sensorMeasurementRepository.fetchLastThreeMeasurements(ANY_UUID) >> previousMeasurements
-//        1 * service.statusCalculator.calculateCurrentStatus(currentMeasurement, previousMeasurements) >> currentStatus
-        1 * service.sensorMeasurementRepository.collect(ANY_UUID, currentStatus)
+        1 * service.sensorMeasurementRepository.collect(ANY_UUID, currentMeasurement)
+    }
+
+    def "recordAndUpdateAlert() No-Alarm: No alert is recorded when the alert limit is not crossed"(
+            SensorMeasurement current,
+            List<SensorMeasurement> previousThree) {
+        when:
+        service.recordAndUpdateAlert(ANY_UUID, MEASUREMENT_BELOW_THRESHOLD)
+
+        then:
+        1 * service.sensorMeasurementRepository.fetchLastThreeMeasurements(ANY_UUID) >> previousThree
+        _ * service.alertRepository.getLatestOngoing(ANY_UUID) >> Optional.empty()
+        0 * service.alertRepository.save(ANY_UUID, _)
+
+        where:
+        current                     | previousThree
+        MEASUREMENT_BELOW_THRESHOLD | []
+        MEASUREMENT_AT_THRESHOLD    | [MEASUREMENT_BELOW_THRESHOLD]
+        MEASUREMENT_ABOVE_THRESHOLD | [MEASUREMENT_BELOW_THRESHOLD] * 3
+
+        MEASUREMENT_BELOW_THRESHOLD | [MEASUREMENT_ABOVE_THRESHOLD]
+
+        MEASUREMENT_BELOW_THRESHOLD | [MEASUREMENT_BELOW_THRESHOLD,
+                                       MEASUREMENT_ABOVE_THRESHOLD,
+                                       MEASUREMENT_ABOVE_THRESHOLD]
 
     }
 }
